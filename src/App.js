ffa4847f77cc20e9382572974e06f70d79125100
Mo-deck <mohamedabow6774@gmail.com>
2024-10-05 09:06:25 +0300

    building our login components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import Login from './components/Login';
+
+
 const App = () => {
+  let user = ""
+
+  if(!user) return <Login />
     return(    
       <h1>App</h1>
     )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react"
+
 const Login = () =>{
-    return<Login/>
+    const [username,setUsername] = useState("")
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(username);
+        
+    }
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="enter your username" 
+                onChange={(event)=>setUsername(event.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
 }
 
 export default Login
